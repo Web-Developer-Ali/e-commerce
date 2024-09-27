e9043c53f6355922ed67e39f859d3837e4f5de8b
Web-Developer-Ali <148641299+Web-Developer-Ali@users.noreply.github.com>
2024-09-27 20:59:37 +0500

    errr

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -51,9 +51,9 @@ export const authOptions: NextAuthOptions = {
   callbacks: {
     async jwt({ token, user , session , trigger }) {
      
-      // if (trigger === 'update' && session?.otpVerified) {
-      //   token.otpVerified = session.otpVerified
-      // }
+      if (trigger === 'update' && session?.otpVerified) {
+        token.otpVerified = session.otpVerified
+      }
       // When a user logs in, we can set the token properties
       if (user) {
         token._id = user._id?.toString();
